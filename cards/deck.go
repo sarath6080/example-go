60c3ba4ea9e805fed8b9b3ca7b62a1397abad7ea
sarath-beldex <sarath.kumar@beldex.org>
2019-05-07 17:23:11 +0530

    generating a random cards from the deck

--- cards/deck.go
+++ cards/deck.go
@@ -4,6 +4,21 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "two", "three", "Four"}
+
+	for _, suits := range cardSuits {
+		for _, Values := range cardValues {
+			cards = append(cards, suits+" of "+Values)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- cards/main.go
+++ cards/main.go
@@ -1,12 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Ace of hearts", newCard()}
-	cards = append(cards, "queen fo spades")
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
